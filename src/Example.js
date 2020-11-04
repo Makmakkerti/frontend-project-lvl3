9d4a844ddecc812d710dcd2d6e37cd9ef1efadba
makmakkerti <makmakkerti007@gmail.com>
2020-11-04 12:48:35 +0200

    Small html changes

--- src/Example.js
+++ src/Example.js
@@ -10,7 +10,7 @@ export default class Example {
                                 <div class="container-xl">
                                   <div class="row">
                                     <div class="col-md-10 col-lg-8 mx-auto text-white">
-                                        <h1 class="display-4">RSS READER</h1>
+                                        <h1 class="display-3">RSS Reader</h1>
                                         <p class="lead">Start reading RSS today! It is easy, it is nicely.</p>
                                         <form action="" class="rss-form"><div class="form-row"><div class="col"><input autofocus="" required="" name="url" aria-label="url" class="form-control form-control-lg w-100" placeholder="RSS link"></div><div class="col-auto"><button type="submit" aria-label="add" class="btn btn-lg btn-primary px-sm-5">Add</button></div></div></form>
                                         <p class="text-muted my-1">Example: https://ru.hexlet.io/lessons.rss</p>
@@ -18,6 +18,5 @@ export default class Example {
                                   </div>
                                 </div>
                               </div>`;
-    console.log('ehu!');
   }
 }
--- src/init.js
+++ src/init.js
@@ -2,8 +2,20 @@
 import 'bootstrap';
 import Example from './Example';
 
+const ft = `<div class="container-xl">
+    <div class="text-center">created by
+    <a href="https://ru.hexlet.io/professions/frontend/projects/11" target="_blank">Hexlet</a>
+    </div>
+  </div>`;
+
+const footer = document.createElement('footer');
+footer.classList.add('footer', 'border-top', 'py-3', 'mt-5');
+footer.innerHTML = ft;
+
 export default () => {
-  const element = document.getElementById('point');
+  const element = document.querySelector('#point');
+  const body = document.querySelector('body');
+  body.appendChild(footer);
   const obj = new Example(element);
   obj.init();
 };
