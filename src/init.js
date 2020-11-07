9dc18957171ba02799879fc0a32cf2583c86802a
makmakkerti <makmakkerti007@gmail.com>
2020-11-07 11:10:33 +0200

    onChange added, watched state on adding rssLink

--- src/init.js
+++ src/init.js
@@ -1,5 +1,6 @@
 // @ts-nocheck
 import 'bootstrap';
+import onChange from 'on-change';
 import * as yup from 'yup';
 import axios from 'axios';
 import Jumbotron from './jumbotron';
@@ -15,10 +16,94 @@ footer.classList.add('footer', 'border-top', 'py-3', 'mt-5');
 footer.innerHTML = footerContent;
 
 const app = () => {
-  const state = {
+  const appState = {
     rssLinks: [],
     feeds: [],
     posts: [],
+  };
+
+  const render = (state) => {
+    const feeds = document.querySelector('.feeds');
+    const posts = document.querySelector('.posts');
+    feeds.innerHTML = '';
+    posts.innerHTML = '';
+
+    const feedsHeading = document.createElement('h2');
+    feedsHeading.textContent = 'Feeds';
+    feeds.appendChild(feedsHeading);
+    const feedList = document.createElement('ul');
+    feedList.classList.add('list-group', 'mb-5');
+
+    state.feeds.forEach((feed) => {
+      const feedItem = document.createElement('li');
+      const head = document.createElement('h3');
+      head.textContent = feed.channelTitle;
+      const desc = document.createElement('p');
+      desc.textContent = feed.channelDescription;
+      feedItem.appendChild(head);
+      feedItem.appendChild(desc);
+      feedItem.classList.add('list-group-item');
+      feedList.appendChild(feedItem);
+    });
+    feeds.appendChild(feedList);
+
+    const postsHeading = document.createElement('h3');
+    postsHeading.textContent = 'Posts';
+    const postList = document.createElement('ul');
+    posts.appendChild(postsHeading);
+    postList.classList.add('list-group', 'mb-5');
+
+    state.posts.forEach((post) => {
+      const postItem = document.createElement('li');
+      const link = document.createElement('a');
+      link.setAttribute('href', post.link);
+      link.textContent = post.title;
+      postItem.classList.add('list-group-item');
+      postItem.appendChild(link);
+      posts.appendChild(postItem);
+    });
+    posts.appendChild(postList);
+  };
+
+  const watchedState = onChange(appState, (path, value, oldValue) => {
+    switch (path) {
+      case 'rssLinks':
+        console.log(value, oldValue);
+        render(appState);
+        break;
+      default:
+        break;
+    }
+  });
+
+  const parse = (parsedXML) => {
+    const channelData = parsedXML.querySelector('channel');
+    const channelTitle = channelData.querySelector('title').textContent;
+    const channelDescription = channelData.querySelector('description').textContent;
+    const channelLink = channelData.querySelector('link').textContent;
+    const channelId = watchedState.feeds.length + 1;
+    watchedState.feeds.push({
+      channelTitle,
+      channelDescription,
+      channelLink,
+      id: channelId,
+    });
+
+    const allPosts = channelData.querySelectorAll('item');
+    let postId = 1;
+    allPosts.forEach((post) => {
+      const title = post.querySelector('title').textContent;
+      const description = post.querySelector('description').textContent;
+      const link = post.querySelector('link').textContent;
+      watchedState.posts.push({
+        title,
+        description,
+        link,
+        channelId,
+        id: postId,
+      });
+      postId += 1;
+    });
   };
 
   const validate = (link) => {
@@ -59,98 +144,25 @@ const app = () => {
       return;
     }
 
-    if (state.rssLinks.includes(url)) {
+    if (watchedState.rssLinks.includes(url)) {
       feedback.classList.add('text-danger');
       formInput.classList.add('border', 'border-danger');
       feedback.textContent = 'RSS already in the list!';
       return;
     }
 
-    const render = (selector) => {
-      const div = document.querySelector(`.${selector.toLowerCase()}`);
-      div.innerHTML = '';
-
-      const heading = document.createElement('h2');
-      heading.textContent = selector;
-      const ul = document.createElement('ul');
-      ul.classList.add('list-group');
-
-      if (selector.toLowerCase() === 'feeds') {
-        ul.classList.add('mb-5');
-        state.feeds.forEach((feed) => {
-          const li = document.createElement('li');
-          const head = document.createElement('h3');
-          head.textContent = feed.channelTitle;
-          const desc = document.createElement('p');
-          desc.textContent = feed.channelDescription;
-          li.appendChild(head);
-          li.appendChild(desc);
-          li.classList.add('list-group-item');
-          ul.appendChild(li);
-        });
-      }
-
-      if (selector.toLowerCase() === 'posts') {
-        state.posts.forEach((post) => {
-          const li = document.createElement('li');
-          const link = document.createElement('a');
-          link.setAttribute('href', post.link);
-          link.textContent = post.title;
-          li.classList.add('list-group-item');
-          li.appendChild(link);
-          ul.appendChild(li);
-        });
-      }
-
-      div.appendChild(heading);
-      div.appendChild(ul);
-    };
-
-    const parse = (parsedXML) => {
-      const channelData = parsedXML.querySelector('channel');
-      const channelTitle = channelData.querySelector('title').textContent;
-      const channelDescription = channelData.querySelector('description').textContent;
-      const channelLink = channelData.querySelector('link').textContent;
-      const channelId = state.feeds.length + 1;
-      state.feeds.push({
-        channelTitle,
-        channelDescription,
-        channelLink,
-        id: channelId,
-      });
-
-      const allPosts = channelData.querySelectorAll('item');
-      let postId = 1;
-      allPosts.forEach((post) => {
-        const title = post.querySelector('title').textContent;
-        const description = post.querySelector('description').textContent;
-        const link = post.querySelector('link').textContent;
-        state.posts.push({
-          title,
-          description,
-          link,
-          channelId,
-          id: postId,
-        });
-        postId += 1;
-      });
-      render('Feeds');
-      render('Posts');
-    };
-
     axios.get(`https://api.allorigins.win/get?url=${url}`).then((response) => {
       const parser = new DOMParser();
       const parsedXML = parser.parseFromString(response.data.contents, 'text/xml');
       parse(parsedXML);
       formInput.value = '';
+      watchedState.rssLinks.push(url);
       feedback.textContent = 'Rss has been loaded';
       feedback.classList.add('text-success');
     })
       .catch((err) => {
         console.log(err);
       });
-
-    state.rssLinks.push(url);
   });
 };
 
