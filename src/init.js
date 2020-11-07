401a91d7c4af4f752d967f0155115c75f821a775
makmakkerti <makmakkerti007@gmail.com>
2020-11-07 21:20:30 +0200

    Form state added, feedback watcher

--- src/init.js
+++ src/init.js
@@ -15,11 +15,25 @@ const footer = document.createElement('footer');
 footer.classList.add('footer', 'border-top', 'py-3', 'mt-5');
 footer.innerHTML = footerContent;
 
+const body = document.querySelector('body');
+body.appendChild(footer);
+
+const element = document.querySelector('#point');
+const obj = new Jumbotron(element);
+obj.init();
+
+const form = document.querySelector('.rss-form');
+const formInput = document.querySelector('.rss-form input');
+const formButton = document.querySelector('.rss-form button');
+const feedback = document.querySelector('.feedback');
+
 const app = () => {
   const appState = {
     rssLinks: [],
     feeds: [],
     posts: [],
+    error: '',
+    formState: '',
   };
 
   const render = (state) => {
@@ -65,11 +79,35 @@ const app = () => {
     posts.appendChild(postList);
   };
 
-  const watchedState = onChange(appState, (path, value, oldValue) => {
+  const watchedState = onChange(appState, (path, value) => {
     switch (path) {
       case 'rssLinks':
-        console.log(value, oldValue);
         render(appState);
+        break;
+      case 'formState':
+        if (value === 'invalid') {
+          console.log('Validation error!');
+        }
+
+        if (value === 'sending') {
+          feedback.classList.remove('text-danger');
+          formInput.classList.remove('border', 'border-danger');
+          formInput.disabled = true;
+          formButton.disabled = true;
+        }
+
+        if (value === 'success') {
+          formInput.disabled = false;
+          formButton.disabled = false;
+          formInput.value = '';
+          feedback.textContent = 'Rss has been loaded';
+          feedback.classList.add('text-success');
+        }
+        break;
+      case 'error':
+        feedback.textContent = value;
+        feedback.classList.add('text-danger');
+        formInput.classList.add('border', 'border-danger');
         break;
       default:
         break;
@@ -106,61 +144,36 @@ const app = () => {
     });
   };
 
-  const validate = (link) => {
-    const schema = yup.string().trim().url().required();
-
-    try {
-      schema.validateSync(link);
-      return null;
-    } catch (err) {
-      return err.message;
-    }
-  };
-
-  // Adding Jumbotron to the page
-  const element = document.querySelector('#point');
-  const body = document.querySelector('body');
-  body.appendChild(footer);
-  const obj = new Jumbotron(element);
-  obj.init();
-  const form = document.querySelector('.rss-form');
-  const formInput = document.querySelector('.rss-form input');
-  const feedback = document.querySelector('.feedback');
-
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-    // @ts-ignore
     const data = new FormData(e.target);
     const url = data.get('url');
-    const validationError = validate(url);
-
-    formInput.classList.remove('border', 'border-danger');
-    feedback.classList.remove('text-danger', 'text-success');
-
-    if (validationError) {
-      formInput.classList.add('border', 'border-danger');
-      feedback.textContent = validationError;
-      feedback.classList.add('text-danger');
-      return;
-    }
+    const schema = yup.string().trim().url().required();
 
     if (watchedState.rssLinks.includes(url)) {
-      feedback.classList.add('text-danger');
-      formInput.classList.add('border', 'border-danger');
-      feedback.textContent = 'RSS already in the list!';
+      watchedState.error = 'RSS already in the list!';
       return;
     }
+
+    try {
+      schema.validateSync(url);
+    } catch (err) {
+      watchedState.error = err.message;
+      return;
+    }
+    watchedState.error = '';
+
+    watchedState.formState = 'sending';
 
     axios.get(`https://api.allorigins.win/get?url=${url}`).then((response) => {
       const parser = new DOMParser();
       const parsedXML = parser.parseFromString(response.data.contents, 'text/xml');
       parse(parsedXML);
-      formInput.value = '';
       watchedState.rssLinks.push(url);
-      feedback.textContent = 'Rss has been loaded';
-      feedback.classList.add('text-success');
+      watchedState.formState = 'success';
     })
       .catch((err) => {
+        watchedState.error = 'Source is not correct!';
         console.log(err);
       });
   });
