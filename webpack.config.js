a3d7dda03bcc281ca03cb248634d45b38491cdd5
makmakkerti <makmakkerti007@gmail.com>
2020-11-14 19:50:07 +0200

    favicon webpack plugin temporarily disabled

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
@@ -29,6 +28,5 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'template.html',
     }),
-    new FaviconsWebpackPlugin('../favicon.ico'),
   ],
 };
